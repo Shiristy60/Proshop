dc4835872c731e614860ddcd2c9d26ecd86f74c3
Shiristy60 <shiristykotnala1.1@gmail.com>
2021-04-17 11:12:18 +0530

    Custom authentication middleware

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,12 +1,12 @@
 // for cleanup
 // main functionality of the routes is handled by these controllers.
 
-import User from '../models/UserModel.js'
+import User from '../models/userModel.js'
 import asyncHandler from 'express-async-handler'
 import generateToken from '../utils/generateToken.js'
 
 // @desc        Auth user & get token
-// @route       post /api/users/login
+// @route       POST /api/users/login
 // @access      Public
 const authUser = asyncHandler(async(req, res) => {
     // get data from body
@@ -22,14 +22,33 @@ const authUser = asyncHandler(async(req, res) => {
             isAdmin: user.isAdmin,
             token: generateToken(user._id)
         })
-    } else { //if wither email or password is incorrect
+    } else { //if either email or password is incorrect
         res.status(401)
         throw new Error('Invalid email or password')
     }
 })
 
+// @desc        Get user profile
+// @route       GET /api/users/profile
+// @access      Private
+const getUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id)
+    if (user) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        })
+    } else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+})
+
 export {
-    authUser
+    authUser,
+    getUserProfile
 }
 
 // import this file in userRoutes.
--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,8 +1,11 @@
 import jwt from 'jsonwebtoken'
 
 const generateToken = (id) => {
+    // sign method is used to generate a token
+    // id - id of the user who logs in 
+    // process.env.JWT_SECRET - secret key
     return jwt.sign({ id }, process.env.JWT_SECRET, {
-        expiresIn: '30d'
+        expiresIn: '30d' // 30 days
     })
 }
 
