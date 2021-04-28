8d52ab05d94bd2e365200ff956d1b157b1aeef9e
Shiristy60 <shiristykotnala1.1@gmail.com>
2021-04-28 11:21:11 +0530

    Adding paypal payments

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,8 @@ app.use('/api/products', productRoutes) // use productRoutes file for any url th
 app.use('/api/users', userRoutes)   // use userRoutes file for any url that goes through /api/users
 app.use('/api/orders', orderRoutes)   // use userRoutes file for any url that goes through /api/users
 
+app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
+
 // middlewares
 app.use(notFound)
 app.use(errorHandler)
