36a262bde625ef001119db918d5be3256e25ab85
Shiristy60 <shiristykotnala1.1@gmail.com>
2021-05-01 23:29:59 +0530

    Admin delete products

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -25,9 +25,24 @@ const getProductById = asyncHandler(async(req, res) => {
     }
 })
 
+// @desc        Delete a product
+// @route       DELETE /api/products/:id
+// @access      Private/Admin
+const deleteProduct = asyncHandler(async(req, res) => {
+    const product = await Product.findById(req.params.id)
+    if (product) {
+        await product.remove()
+        res.json({message: 'Product removed'})
+    } else {
+        res.status(404)
+        throw new Error('Product not found')
+    }
+})
+
 export {
     getProductById,
-    getProducts
+    getProducts,
+    deleteProduct
 }
 
 // import this file in productRoutes.
